ea413938009c68a1e8d1b61ef5f787d60e3d9371
ts331117 <ts331117@ohio.edu>
2020-01-24 15:01:57 -0500

    Hello Gitgit add git-lab-program.cc

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+         *        File: git-lab-program.cc
+         *      Author: Trent Sherrod
+         *        Date: Jan 24th 2020
+         * Description: 
+         */
+
+        #include <iostream>
+        #include <iomanip>
+        #include <cstdlib>
+        using namespace std;
+
+
+        int main(int argc, char const *argv[]) {
+
+            cout << "Hello Git!!" << endl;
+
+            return 0;
+        }// main
